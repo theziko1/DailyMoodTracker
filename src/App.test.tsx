1073d33db6ea864fbbbb34060c2ca0a9e4cb9161
theziko1 <zakariaghachim96@gmail.com>
2024-03-12 11:10:49 +0000

    init of project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,1 @@
 
-import { render, fireEvent } from '@testing-library/react';
-import App from './App';
-
-test('submitting the mood form displays emoji', () => {
-  const { getByPlaceholderText, getByTestId } = render(<App />);
-  const input = getByPlaceholderText('Write Your emotion you feel !');
-  const submitButton = getByTestId('button-icon');
-
-  fireEvent.change(input, { target: { value: 'happy' } });
-  fireEvent.click(submitButton);
-
-  const emojiElement = getByTestId('emoji-display');
-  expect(emojiElement.textContent).toBe('😊');
-});
